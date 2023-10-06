a498ccb498bd53d568d5b9ae71988016e128cc3e
CHO <202044015@itc.ac.kr>
2023-10-06 23:03:28 +0900

    v0

--- week05/main.go
+++ week05/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-
+	"bufio"
 	"fmt"
-	"time"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 )
 
+/*
 func main() {
-	/*
+
 	var now time.Time
 	now = time.Now()
 	year := now.Year()
 	var month string = now.Month().String()
 	fmt.Println(year, month, now.Hour(), now.Minute(), now.Second())
-	
+
 
 	HotSpur := "hm ? j madi?"
 	replacePlayer := strings.NewReplayer("?","son")
 	player := replacePlayer.Replace(HotSpur)
 	fmt.Println(player)
-	*/
-	/*
+*/
+/*
 	fmt.Print("Input score:")
 	reader := bufio.newReader(os.Stdin)
 	InputScore := reader.ReadString('\n')
@@ -41,8 +46,8 @@ func main() {
 
 	dice := reand.Intn(6) + 1
 	fmt.Println(dice)
-	*/
-
+*/
+/*
 	func main() {
 		rand.Seed(time.Now().Unix()) //get the current data and time as an integer
 		answer := rand.Intn(100) + 1 // random integer number (1~100)
@@ -58,7 +63,7 @@ func main() {
 			fmt.Print("Input guess number")
 			inputNumberString, err := reader.ReadString('\n')
 			if err != nil {
-	
+
 				log.Fatal(err)
 			}
 			inputNumberString = strings.TrimSpace(inputNumberString)
@@ -72,7 +77,37 @@ func main() {
 				fmt.Println("Guess number is lower than answer") // The answer is higher!
 			} else if inputNumber > answer {
 				fmt.Println("Guess number is higher than answer") // The answer is lower!
-			} 
-		}	
+			}
+		}
+	}
+
+}
+*/
+func main() {
+	fmt.Println("Guess number game!")
+	answer := rand.Intn(100) + 1 // 1 ~ 100
+	fmt.Println(answer)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("Chance left :", 10-i)
+		fmt.Print("Input number : ")
+		reader := bufio.NewReader(os.Stdin)
+		inputGuessString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputGuessString = strings.TrimSpace(inputGuessString) // remove space
+		inputGuess, err := strconv.Atoi(inputGuessString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if inputGuess == answer {
+			fmt.Println("Great! You got the number. Congratulations~")
+			break
+		} else if inputGuess < answer {
+			fmt.Println("Your guess number is lower than answer!") // answer is higher
+		} else if inputGuess > answer {
+			fmt.Println("Your guess number is higher than answer!") // answer is lower
+		}
 	}
 }
