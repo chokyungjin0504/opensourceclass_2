32249a830c09d0cce6b53fd3db9774310c68ab60
Cho <202044015@itc.ac.kr>
2023-10-23 14:55:25 +0900

    v1.7

--- week09/week09.go
+++ week09/week09.go
@@ -2,12 +2,29 @@ package main
 
 import "fmt"
 
-func double(n *int) {
-	*n = *n * 2
+func sum(n1 int, n2 int) {
+	temp := n1
+	n1 = n2
+	n2 = temp
 }
 
 func main() {
-	var a int = 6
-	double(&a)
-	fmt.Println(a)
+	a := 10
+	//b := 20 //var b int == 20
+
+	c := &a
+	fmt.Printf("%T\n", c)
+	fmt.Println(a, b)
+	//swap(a,b)
+	fmt.Println(a, b)
 }
+
+// func double(n *int) {
+// 	*n = *n * 2
+// }
+
+// func main() {
+// 	var a int = 6
+// 	double(&a)
+// 	fmt.Println(a)
+// }
