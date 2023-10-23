7b5061064ffcbd3d77226138ecb541873b71c534
Cho <202044015@itc.ac.kr>
2023-10-23 14:58:10 +0900

    v1.8

--- week09/week09.go
+++ week09/week09.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func sum(n1 int, n2 int) {
-	temp := n1
-	n1 = n2
-	n2 = temp
+func swap(n1 *int, n2 *int) {
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
 }
 
 func main() {
 	a := 10
-	//b := 20 //var b int == 20
+	b := 20
 
 	c := &a
 	fmt.Printf("%T\n", c)
 	fmt.Println(a, b)
-	//swap(a,b)
+	swap(&a, &b)
 	fmt.Println(a, b)
 }
 
